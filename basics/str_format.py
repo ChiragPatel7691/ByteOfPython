e457d3b20e9002d6ad7c6a47594f156ffb2f55c6
Chirag Patel <chiragpatel7778@gmail.com>
2024-04-25 19:22:31 +0530

    Added diff print method in str_format.py

--- basics/str_format.py
+++ basics/str_format.py
@@ -3,3 +3,12 @@ name = 'Swaroop'
 
 print('{0} was {1} years old when he wrote this book'.format(name, age))
 print('Why is {0} playing with that python?'.format(name))
+
+print('{} was {} years old when he wrote this book'.format(name, age))
+print('Why is {} playing with that python?'.format(name))
+
+print('{name} was {age} years old when he wrote this book'.format(name=name, age=age))
+print('Why is {name} playing with that python?'.format(name=name))
+
+print(f'{name} was {age} years old when he wrote this book') # notice the 'f' before the string
+print(f'Why is {name} playing with that python?') # notice the 'f' before the string
